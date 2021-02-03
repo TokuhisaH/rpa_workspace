eb29252b5fbf6ec02e1877390e2df77ddfad3cb8
TokuhisaH <tokuhisahiroki@gmail.com>
2021-02-03 19:48:24 +0900

    test pyautogui

---
+++ pyautogui_demo.py
@@ -0,0 +1,27 @@
+import pyautogui as pag
+
+#画面のサイズ取得、原点は左上
+#print("画面サイズ",pag.size())
+
+#scr_w,scr_h=pag.size()
+
+#print(scr_w,scr_h)
+
+#画面中央
+#cent_x = scr_w/2
+#cent_y = scr_h/2
+
+#print("画面の中心の座標",cent_x,cent_y)
+
+#マウスカーソルの位置
+#print("マウスカーソルの位置",pag.position())
+
+#m_posi_x,m_posi_y = pag.position()
+#print("現在のマウス位置 x:",m_posi_x,"y:",m_posi_y)
+
+try:
+    while True:
+        m_posi_x,m_posi_y = pag.position()
+        print("現在のマウス位置 x:",m_posi_x,"y:",m_posi_y)
+except KeyboardInterrupt:
+    print("\n")
